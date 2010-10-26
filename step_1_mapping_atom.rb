68c9f2cc7e91b0861c1401af820f318ffc971866
Yufeng Shen % I. Pe'er <joshuashen@gmail.com>
2010-10-26 16:39:46 -0400

    minor changes of bwa options

--- step_1_mapping_atom.rb
+++ step_1_mapping_atom.rb
@@ -6,6 +6,8 @@
 
 ### Note: we probably want to change some bwa parameters: 
  ##  number of allowed gaps in "aln" step: -o 2 (default is 1) 
+ ##  Parameter for read trimming in "aln" step: -q 5 (default is 0), it trimmed from the 3' end
+   ## until most bases have quality > INT.  argmax_x{\sum_{i=x+1}^l(INT-q_i)} 
 
 require 'getoptlong'
 
@@ -17,6 +19,7 @@ def main
      ["--pair", "-p", GetoptLong::OPTIONAL_ARGUMENT],
      ["--help", "-h", GetoptLong::NO_ARGUMENT],
      ["--gaps", "-g", GetoptLong::OPTIONAL_ARGUMENT],
+     ["--qualtrim", "-q", GetoptLong::OPTIONAL_ARGUMENT],
      ["--bwa", "-b", GetoptLong::OPTIONAL_ARGUMENT]
   )
   
@@ -26,7 +29,7 @@ def main
   end
   
   if optHash.key?("--help")
-    $stderr.puts "Usage: ruby __.rb -i foo.1.fastq [-p foo.2.fastq] [-r reference.fasta]"
+    $stderr.puts "Usage: ruby __.rb -i foo.1.fastq [-p foo.2.fastq] [-r reference.fasta] [-q quality_trim] [-g maxgaps]"
     $stderr.puts "Note: mapping paired reads requires -p argument."
     exit
   end
@@ -61,9 +64,13 @@ def main
   if optHash.key?("--gaps")
       maxgaps = optHash["--gaps"].to_i
   end
-
+ 
+  qualtrim = 5
+  if optHash.key?("-qualtrim")
+      qualtrim = optHash["--qualtrim"].to_i
+ end
   ######### align step
-  cmd="#{bwa} aln -o #{maxgaps} -t  #{threads} #{options}  #{ref} #{f1} > #{f1}.sai" 
+  cmd="#{bwa} aln -q #{qualtrim} -o #{maxgaps} -t  #{threads} #{options}  #{ref} #{f1} > #{f1}.sai" 
   
   system(cmd)
 
