4c39ba9c96c60fa2c3a510ae8c94199f6115b33c
Angad P. Singh % Itsik <aps2157@b1a.c2.titan>
2010-11-24 19:16:00 +0000

    Modified the scripts to merge the files right after realignment. Bunch of changes for portability and others

--- pipeline/job.sh
+++ pipeline/job.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 #$ -cwd
-#$ -l mem=15G,time=16::
 
-rm *.output
+GLOBAL="global_config.sh"
 
-./run_pipeline.sh -I /ifs/scratch/c2b2/ip_lab/yshen/Yale/$1 -R /ifs/data/c2b2/ip_lab/shares/DATA/Sequencing/resources/bcm_hg18.fasta -E /ifs/scratch/c2b2/ip_lab/aps2157/ExomePipe/pipeline/data/old_samples/captureexoncoordinatesonly.txt -D /ifs/scratch/c2b2/ip_lab/aps2157/ExomePipe/pipeline/data/old_samples/dbsnp_129_hg18.ordered.rod > pipeline.output
+if [[ -e $GLOBAL ]]
+then
+	. $GLOBAL
+else
+        echo "Global config file not found. Exiting."
+        exit 1
+fi
+
+if [[ $1 == "" ]]
+then
+	echo "Please pass input bam file as parameter"
+#	exit 1
+fi
+
+INPUT="/ifs/scratch/c2b2/ip_lab/yshen/Yale/RNAseq19082010/bamfiles/0/s_10242_1_sequence.txt.bam.sorted.bam"
+DATAPATH=`dirname "$INPUT"`
+rm -f $DATAPATH/*.output
+
+#BPATH/run_pipeline.sh -I $1 -R $WU_REF -E $ExonList -D $WU_DBSNP -P $WU_Platform > $DATAPATH/pipeline.output 
+$BPATH/run_pipeline.sh -I $INPUT -R $YALE_REF -E $ExonList -D $YALE_DBSNP -P $YALE_Platform > $DATAPATH/pipeline.output
 
--- pipeline/run_pipeline.sh
+++ pipeline/run_pipeline.sh
@@ -1,208 +1,308 @@
 #!/bin/sh
 #$ -cwd
 
-INP=""
-CHR=""
-REF=""
-DBSNP=""
-ExonFile=""
+GLOBAL="global_config.sh"
 
-USAGE="Usage: $0 -I <Input bam file> -R <Reference fasta> -D <DBSNP file> [[-L #:#-#] | [-E <ExonFile>]]"
-ERRORMESSAGE="#### ERROR"
-ERRORMESSAGE1="The following error has occurred"
+if [[ -e $GLOBAL ]]
+then
+        . $GLOBAL
+else
+	echo "Global config file not found. Exiting."
+	exit 1
+fi
 
-while getopts I:L:R:D:E:h o
+USAGE="Usage: run_pipeline.sh -I <Input bam file> -R <Reference fasta> -D <DBSNP file> -P <Platform> [[-L #:#-#] | [-E <ExonFile>]]"
+
+while getopts I:L:R:D:P:E:h o
 do      case "$o" in
         I)      INP="$OPTARG";;
         L)      CHR="$OPTARG";;
         R)      REF="$OPTARG";;
         D)      DBSNP="$OPTARG";;
+        P)      Platform="$OPTARG";;
         E)      ExonFile="$OPTARG";;
 	h)	echo $USAGE
 		exit 1;;
         esac
 done
 
-if [[ $INP == "" || $REF == "" || $DBSNP == "" ]]
+if [[ $INP == "" || $REF == "" || $DBSNP == "" || $Platform == "" ]]
 then
 	echo $USAGE
 	exit 1
 fi
 
+SUFFIX=".fixed.bam"
+DATAPATH=`dirname "$INP"`
+CONTIG_ORDER="`$SAMTOOLS idxstats $INP | grep -m 24 [0-9] | awk '{print $1}' | tr '\n' ' '`"
+CONTIG_INP_ORDER="`$SAMTOOLS idxstats $INP | grep -m 24 [0-9] | awk -v var1="$INP" -v var2="$SUFFIX" '{print var1 $1 var2}' | tr '\n' ' '`"
+RESULT=0
 
+if [[ $CHR == "" && $ExonFile == "" ]]
+then
+	echo $EXONUSAGE
+	exit 1
+fi
+
+while [[ $RESULT == 0 ]]
+do
 echo
 date
 echo "Calling ./gatk_realign.scr step by step..."
-
+RESULT=1
 if [[ $CHR == "" ]]
 then
-	qsub -l mem=8G,time=6:: -e realignment.output -o realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP -L "X"
-	qsub -l mem=8G,time=6:: -e realignment.output -o realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP -L "Y"
-	qsub -l mem=8G,time=16:: -t 1-22 -sync y -e realignment.output -o realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP
+	qsub -l mem=5G,time=2:: -e $DATAPATH/realignment.output -o $DATAPATH/realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP -L "X"
+	sleep 10
+	qsub -l mem=5G,time=2:: -e $DATAPATH/realignment.output -o $DATAPATH/realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP -L "Y"
+	sleep 10
+	RET=`qsub -l mem=8G,time=16:: -t 21 -e $DATAPATH/realignment.output -o $DATAPATH/realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP | awk '{print $3}'`
 else
-	qsub -l mem=8G,time=16:: -sync y -o realignment.output -e realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP -L $CHR
+	RET=`qsub -l mem=8G,time=16:: -o $DATAPATH/realignment.output -e $DATAPATH/realignment.output ./gatk_realign.scr -I $INP -R $REF -D $DBSNP -L $CHR | awk '{print $3}'`
 fi
-if [[ $? != 0 || `grep "$ERRORMESSAGE" realignment.*.output pipeline.output` != "" ]]
+
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running realigner...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/realignment*.output "$DATAPATH"/pipeline.output` != "" ]]
 then
 	echo "Realignment FAILED"
-	exit 1
+	RESULT=0
 fi
+done
 echo "Execution of ./gatk_realign.scr complete"
+RESULT=0
 echo
 
 
 
-date
-echo "Calling ./gatk_calibrate.scr ..."
-if [[ $CHR == "" ]]
-then
-	qsub -l mem=8G,time=6:: -o calibrate.output -e calibrate.output ./gatk_calibrate.scr -I $INP -R $REF -D $DBSNP -L "X"
-	qsub -l mem=8G,time=6:: -o calibrate.output -e calibrate.output ./gatk_calibrate.scr -I $INP -R $REF -D $DBSNP -L "Y"
-	qsub -l mem=8G,time=16:: -t 1-22 -sync y -o calibrate.output -e calibrate.output ./gatk_calibrate.scr -I $INP -R $REF -D $DBSNP
-else
-	qsub -l mem=8G,time=16:: -sync y -o calibrate.output -e calibrate.output ./gatk_calibrate.scr -I $INP -R $REF -D $DBSNP -L $CHR
-fi
-if [[ $? != 0 || `grep "$ERRORMESSAGE" calibration.*.output pipeline.output` != "" ]]
-then
-	echo "Calibration FAILED"
-	exit 1
-fi
-echo "Execution of ./gatk_calibrate.scr complete"
-echo
-
-
-
-date
-echo "Calling ./gatk_recalibrate.scr ..."
-if [[ $CHR == "" ]]
-then
-	qsub -l mem=8G,time=6:: -o recalibrate.output -e recalibrate.output ./gatk_recalibrate.scr -I $INP -R $REF -L "X"
-	qsub -l mem=8G,time=6:: -o recalibrate.output -e recalibrate.output ./gatk_recalibrate.scr -I $INP -R $REF -L "Y"
-	qsub -l mem=8G,time=16:: -t 1-22 -sync y -o recalibrate.output -e recalibrate.output ./gatk_recalibrate.scr -I $INP -R $REF
-else
-	qsub -l mem=8G,time=16:: -sync y -o recalibrate.output -e recalibrate.output ./gatk_recalibrate.scr -I $INP -R $REF -L $CHR
-fi
-if [[ $? != 0 || `grep "$ERRORMESSAGE" recalibration.*.output pipeline.output` != "" ]]
-then
-	echo " Recalibration FAILED"
-	exit 1
-fi
-echo "Execution of ./gatk_recalibrate.scr complete"
-echo
-
-
-
-date
-echo "Calling ./gatk_depthofcoverage.scr ..."
-if [[ $ExonFile == "" ]]
-then
-	qsub -l mem=8G,time=8:: -o depthofcoverage.output -e depthofcoverage.output ./gatk_depthofcoverage.scr -I $INP -R $REF -L $CHR
-else
-	qsub -l mem=8G,time=8:: -o depthofcoverage.output -e depthofcoverage.output ./gatk_depthofcoverage.scr -I $INP -R $REF -E "$ExonFile" -L "X"
-	qsub -l mem=8G,time=8:: -o depthofcoverage.output -e depthofcoverage.output ./gatk_depthofcoverage.scr -I $INP -R $REF -E "$ExonFile" -L "Y"
-	qsub -l mem=8G,time=8:: -t 1-22 -o depthofcoverage.output -e depthofcoverage.output ./gatk_depthofcoverage.scr -I $INP -R $REF -E "$ExonFile"
-fi
-if [[ $? != 0 || `grep "$ERRORMESSAGE" depthofcoverage.*.output pipeline.output` != "" ]]
-then
-        echo "Depth of coverage FAILED"
-        exit 1
-fi
-echo "Execution of ./gatk_depthofcoverage.scr complete"
-echo
-
-
-
-date
-echo "Calling ./gatk_indelcall.scr ..."
-#rm $INP.indels.raw.bed
-#rm $INP.detailed.output.bed
-#rm $INP.output.vcf
-if [[ $CHR == "" ]]
-then
-	qsub -l mem=8G,time=6:: -o indelcalling.output -e indelcalling.output ./gatk_indelcall.scr -I $INP -R $REF -L "X"
-	qsub -l mem=8G,time=6:: -o indelcalling.output -e indelcalling.output ./gatk_indelcall.scr -I $INP -R $REF -L "Y"
-	qsub -l mem=8G,time=16:: -t 1-22 -sync y -o indelcalling.output -e indelcalling.output ./gatk_indelcall.scr -I $INP -R $REF
-else
-	qsub -l mem=8G,time=16:: -sync y -o indelcalling.output -e indelcalling.output ./gatk_indelcall.scr -I $INP -R $REF -L $CHR
-fi
-if [[ $? != 0 || `grep "$ERRORMESSAGE" indelcalling.*.output pipeline.output` != "" ]]
-then
-	echo "Indel calling FAILED"
-	exit 1
-fi
-echo "Execution of ./gatk_indelcall.scr complete"
-echo
-
-
-
-date
-echo "Calling ./gatk_snpcall.scr ..."
-if [[ $CHR == "" ]]
-then
-	qsub -l mem=8G,time=6:: -o snpcalling.output -e snpcalling.output ./gatk_snpcall.scr -I $INP -R $REF -D $DBSNP -L "X"
-	qsub -l mem=8G,time=6:: -o snpcalling.output -e snpcalling.output ./gatk_snpcall.scr -I $INP -R $REF -D $DBSNP -L "Y"
-	qsub -l mem=8G,time=16:: -t 1-22 -sync y -o snpcalling.output -e snpcalling.output ./gatk_snpcall.scr -I $INP -R $REF -D $DBSNP
-else
-	qsub -l mem=8G,time=16:: -sync y -o snpcalling.output -e snpcalling.output ./gatk_snpcall.scr -I $INP -R $REF -D $DBSNP -L $CHR
-fi
-if [[ $? != 0 || `grep "$ERRORMESSAGE" snpcalling.*.output pipeline.output` != "" ]]
-then
-	echo "SNP calling FAILED"
-	exit 1
-fi
-echo "Execution of ./gatk_snpcall.scr complete"
-echo
-
-
-
+while [[ $RESULT == 0 ]]
+do
 date
 echo "Calling ./gatk_merge.scr ..."
-qsub -l mem=2G,time=1:: -sync y -o merge.output -e merge.output ./gatk_merge.scr -I $INP -R $REF
-if [[ $? != 0 || `grep "$ERRORMESSAGE" merge.output pipeline.output` != "" ]]
+RESULT=1
+if [[ $CHR == "" ]]
 then
-        echo "Merging of vcf files FAILED"
-        exit 1
+	RET=`qsub -l mem=2G,time=4:: -o $DATAPATH/merge.output -e $DATAPATH/merge.output ./gatk_merge.scr -I $INP.fixed.bam -R $REF -C "$CONTIG_ORDER" -O "$CONTIG_INP_ORDER" | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running merge operation...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/merge.output "$DATAPATH"/pipeline.output` != "" ]]
+then
+        echo "Merging of realigned bam files FAILED"
+	RESULT=0
 fi
+fi
+done
 echo "Execution of ./gatk_merge.scr complete"
+RESULT=0
 echo
 
 
 
+while [[ $RESULT == 0 ]]
+do
+date
+echo "Calling ./gatk_calibrate.scr ..."
+RESULT=1
+RET=`qsub -l mem=8G,time=16:: -o $DATAPATH/calibrate.output -e $DATAPATH/calibrate.output ./gatk_calibrate.scr -I $INP.fixed.bam -R $REF -D $DBSNP -L $CHR | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running Calibration...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/calibrate.output "$DATAPATH"/pipeline.output` != "" ]]
+then
+	echo "Calibration FAILED"
+	RESULT=0
+fi
+done
+echo "Execution of ./gatk_calibrate.scr complete"
+RESULT=0
+echo
+
+
+
+while [[ $RESULT == 0 ]]
+do
+date
+echo "Calling ./gatk_recalibrate.scr ..."
+RESULT=1
+RET=`qsub -l mem=8G,time=16:: -o $DATAPATH/recalibrate.output -e $DATAPATH/recalibrate.output ./gatk_recalibrate.scr -I $INP.fixed.bam -R $REF -L $CHR | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running Recalibration...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/recalibrate.output "$DATAPATH"/pipeline.output` != "" ]]
+then
+	echo " Recalibration FAILED"
+	RESULT=0
+fi
+done
+echo "Execution of ./gatk_recalibrate.scr complete"
+RESULT=0
+echo
+
+
+
+while [[ $RESULT == 0 ]]
+do
+date
+echo "Calling ./gatk_depthofcoverage.scr ..."
+RESULT=1
+if [[ $ExonFile == "" ]]
+then
+	qsub -l mem=8G,time=8:: -o $DATAPATH/depthofcoverage.output -e $DATAPATH/depthofcoverage.output ./gatk_depthofcoverage.scr -I $INP.fixed.bam.recalibrated.bam -R $REF -L $CHR
+else
+	qsub -l mem=8G,time=8:: -o $DATAPATH/depthofcoverage.output -e $DATAPATH/depthofcoverage.output ./gatk_depthofcoverage.scr -I $INP.fixed.bam.recalibrated.bam -R $REF -E "$ExonFile"
+fi
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/depthofcoverage.output "$DATAPATH"/pipeline.output` != "" ]]
+then
+        echo "Depth of coverage FAILED"
+	echo Running Depth Coverage...
+	RESULT=0
+fi
+done
+echo "Execution of ./gatk_depthofcoverage.scr complete"
+RESULT=0
+echo
+
+
+
+while [[ $RESULT == 0 ]]
+do
+date
+echo "Calling ./gatk_indelcall.scr ..."
+RESULT=1
+RET=`qsub -l mem=8G,time=16:: -o $DATAPATH/indelcalling.output -e $DATAPATH/indelcalling.output ./gatk_indelcall.scr -I $INP.fixed.bam.recalibrated.bam -R $REF -L $CHR | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running Indel Calling...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/indelcalling.output "$DATAPATH"/pipeline.output` != "" ]]
+then
+	echo "Indel calling FAILED"
+	RESULT=0
+fi
+done
+echo "Execution of ./gatk_indelcall.scr complete"
+RESULT=0
+echo
+
+
+
+while [[ $RESULT == 0 ]]
+do
+date
+echo "Calling ./gatk_snpcall.scr ..."
+RESULT=1
+RET=`qsub -l mem=8G,time=16:: -o $DATAPATH/snpcalling.output -e $DATAPATH/snpcalling.output ./gatk_snpcall.scr -I $INP.fixed.bam.recalibrated.bam -R $REF -D $DBSNP -P $Platform -L $CHR | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running SNP Calling...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/snpcalling.output "$DATAPATH"/pipeline.output` != "" ]]
+then
+	echo "SNP calling FAILED"
+	RESULT=0
+fi
+done
+echo "Execution of ./gatk_snpcall.scr complete"
+RESULT=0
+echo
+
+
+
+while [[ $RESULT == 0 ]]
+do
 date
 echo "Calling ./gatk_varianteval.scr ..."
-qsub -l mem=5G,time=1:: -sync y -o varianteval_prefiltering.output -e varianteval_prefiltering.output ./gatk_varianteval.scr -I $INP -R $REF -D $DBSNP
-if [[ $? != 0 || `grep "$ERRORMESSAGE" varianteval_prefiltering.output pipeline.output` != "" ]]
+RESULT=1
+RET=`qsub -l mem=5G,time=1:: -o $DATAPATH/varianteval_prefiltering.output -e $DATAPATH/varianteval_prefiltering.output ./gatk_varianteval.scr -I $INP.fixed.bam.recalibrated.bam -R $REF -D $DBSNP | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running Prefilter Variant Evaluation...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/varianteval_prefiltering.output "$DATAPATH"/pipeline.output` != "" ]]
 then
 	echo "Prefilter variant eval calling FAILED"
-	exit 1
+	RESULT=0
 fi
+done
 echo "Execution of ./gatk_varianteval.scr complete"
+RESULT=0
 echo
 
 
 
+while [[ $RESULT == 0 ]]
+do
 date
 echo "Calling ./gatk_filter.scr ..."
-qsub -l mem=5G,time=1:: -sync y -o variantfiltering.output -e variantfiltering.output ./gatk_filter.scr -I $INP -R $REF
-if [[ $? != 0 || `grep "$ERRORMESSAGE" variantfiltering.output pipeline.output` != "" ]]
+RESULT=1
+RET=`qsub -l mem=5G,time=1:: -o $DATAPATH/variantfiltering.output -e $DATAPATH/variantfiltering.output ./gatk_filter.scr -I $INP.fixed.bam.recalibrated.bam -R $REF | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running Variant Filtration...
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/variantfiltering.output "$DATAPATH"/pipeline.output` != "" ]]
 then
 	echo "Variant filtering FAILED"
-	exit 1
+	RESULT=0
 fi
+done
 echo "Execution of ./gatk_filter.scr complete"
+RESULT=0
 echo
 
 
 
+while [[ $RESULT == 0 ]]
+do
 date
 echo "Calling ./gatk_varianteval.scr ..."
-qsub -l mem=5G,time=1:: -sync y -o varianteval_postfiltering.output -e varianteval_postfiltering.output ./gatk_varianteval.scr -I $INP -R $REF -D $DBSNP
-if [[ $? != 0 || `grep "$ERRORMESSAGE" varianteval_postfiltering.output pipeline.output` != "" ]]
+RESULT=1
+RET=`qsub -l mem=5G,time=1:: -o $DATAPATH/varianteval_postfiltering.output -e $DATAPATH/varianteval_postfiltering.output ./gatk_varianteval.scr -I $INP.fixed.bam.recalibrated.bam -R $REF -D $DBSNP | awk '{print $3}'`
+STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+while [[ -z $STATUS ]]
+do
+	STATUS=`qstat -j "$RET" 2>&1 | grep "$JOB_STAT"`
+	echo Running Postfilter Variant Evaluation
+	sleep 10
+done
+if [[ $? != 0 || `grep "$ERRORMESSAGE" "$DATAPATH"/varianteval_postfiltering.output "$DATAPATH"/pipeline.output` != "" ]]
 then
 	echo "Postfilter variant eval calling FAILED"
-	exit 1
+	RESULT=0
 fi
+done
 echo "Execution of ./gatk_varianteval.scr complete"
+RESULT=0
 echo
 date
 
