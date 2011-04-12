798464d8e5db3419c6ad261dbeb8e335ae955d9d
Joshua Shen <joshuashen@gmail.com>
2011-04-12 18:37:25 -0400

    parabayes code: working but may have a bug

--- association/parabayes.R
+++ association/parabayes.R
@@ -22,12 +22,13 @@ library("MCMCpack")
 
 
 ### Note: assume additive model
-function <- dplikelihood(genotable) 
+dplikelihood <- function(gt) 
 {
-	gt <- read.table(genotable, sep="\t", header=T)
-	attach(gt)
+	# gt <- read.table(genotable, sep="\t", header=F)
+	# ## attach(gt)
+	
 ### format of genotable: 
-###  varID, c1, c0, n1, n0, score etc
+###  geneID, varID, c1, c0, n1, n0, score etc
 ## c1: number of alleles in cases; c0: number of alleles in controls; 
 ## n1: number of cases; n0: number of controls
 ## score: prior information. could be useful later
@@ -36,6 +37,7 @@ function <- dplikelihood(genotable)
 	numVar = nrow(gt)
 	
 	p = matrix(rep(1,(numVar+1)*(numVar+2)), nrow=numVar+2, ncol=numVar+1)
+	lratio = rep(0,numVar)
 	# loop through variants
 	p[1,] = 0 ## p(-1,) = 0
 	for (v in 1:numVar) 
@@ -44,56 +46,75 @@ function <- dplikelihood(genotable)
 		for (d in 0:v) 
 		{	
 			dindex = d + 2 
-			p[dindex, vindex]=p[dindex - 1, pvindex -1]*palter(gt[v,]) + p[dindex,vindex-1]*pnull(gt[v,])
+			## print(d)
+			l0 = pnull(as.numeric(gt[v,3:6])) 
+			l1 = palter(as.numeric(gt[v,3:6])) 
+			
+			p[dindex, vindex]=p[dindex - 1, vindex -1]*l1 + p[dindex,vindex-1]*l0			
 			
 		}
 	}
 	
-	## p[numVar+2,] is the final likelihood of each v
+	## p[,numVar+2] is the final likelihood of each v
 
+	## maximum likelihood ratio:
+	for (d in 1:numVar)
+	{	dindex = d + 2
+		lratio[d] = p[dindex, numVar+1] / p[2 ,numVar +1]
+	}
+	print(lratio)
 	### compute the posterior probability of data under alternative hypothesis (d>0)
 	
 
 }
 
-function <- pnull(gvector)
+pnull <- function(gvector)
 {
-	return dnoncenhypergeom(gvector[2], gvector[4], gvector[5], gvector[2]+gvector[3], 1) # OR=1 for null
+	### OR=1 for null
+	p = dnoncenhypergeom(gvector[1], gvector[3], gvector[4], gvector[1] + gvector[2], 1) 
+	p
 }
 
-function <- palter(gvector)
+palter <- function(gvector)
 {	## question: do we want to make OR depends on MAF?  intuitively we should
 	# prior probability of OR:  Gaussian  (Stephens and Balding), with variance 
 	# depends on allele frequency: N(0,t),  t = 1/(p*(1-p))^0.5 / C, C is a scaling factor (C=20 seems reasonable)
 	
 	# make a vector containing OR and prior
-	c = 10  ## scaling factor for prior variantce of effect. 
+	c = 15  ## scaling factor for prior variantce of effect. 
 	### c=15 -> t(freq=0.1) = 0.22, which is close to the default value (0.2) of SNPTEST.
 	### c=10 -> t(freq=0.1) = 0.33
   
 	### We COULD take care of uncertainties in genotype calling or missing data by integral over other possible 
  	### genotypes . 
 
-	freq = (gvector[2] + gvector[3]) / (gvector[4] + gvector[5])	##combined frequency in cases and controls
-	t = 1/(freq * (1-freq))^0.5 / c
+	freq = (gvector[1] + gvector[2]) / (gvector[3] + gvector[4])	##combined frequency in cases and controls
+	tao = 1/(freq * (1-freq))^0.5 / c
 
 	# only calculate approximate posterior likelihood.	
 	## divide into bins of OR, then 
 	# orarray = exp(rnorm(100000, 0, t))
-	binmax = 10 * t
-	binmin = -10 * t
+	## alternatively, we can use Laplace technique to estimate
+	
+	binmax = 10 * tao
+	binmin = -10 * tao
 	bins = 500
 	barray = c(0:bins) * (binmax - binmin)/bins + binmin
-	density = dnorm(barray, 0, t)
+	density = dnorm(barray, 0, tao)
 	totalp = 0
 	for (i in 1:length(barray))
 	{
 		or = exp(barray[i])
-		lh = dnoncenhypergeom(gvector[2], gvector[4], gvector[5], gvector[2]+gvector[3],or)
+		lh = dnoncenhypergeom(gvector[1], gvector[3], gvector[4], gvector[1]+gvector[2],or)
 		totalp = totalp + lh * density[i]
 	}	
+
+##	totalp = totalp - dnoncenhypergeom(gvector[1], gvector[3], gvector[4], gvector[1]+gvector[2],1) * dnorm(0,0,tao)
+
 	totalp = totalp / sum(density)   
-	return totalp
+	
+		
+	totalp
 
 }
 
